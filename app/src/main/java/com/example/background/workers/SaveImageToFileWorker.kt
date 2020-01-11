ac8262aee013f76ca77de05ad95550da292ef1b1
최혁재 <gurwo1007@gmail.com>
2020-01-11 16:40:02 +0900

    Blur처리된 이미지를 기기에 저장하는 Work를 실행할 SaveImageToFileWorker 추가

---
+++ app/src/main/java/com/example/background/workers/SaveImageToFileWorker.kt
@@ -0,0 +1,50 @@
+package com.example.background.workers
+
+import android.content.ContentValues
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.provider.MediaStore
+
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import androidx.work.workDataOf
+import com.example.background.KEY_IMAGE_URI
+import com.example.background.OUTPUT_PATH
+import timber.log.Timber
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
+
+class SaveImageToFileWorker(context: Context, params: WorkerParameters): Worker(context, params) {
+
+    private val TAG by lazy { SaveImageToFileWorker::class.java.simpleName }
+    private val title = "Blurred Image"
+    private val dateFormatter by lazy { SimpleDateFormat("yyyy.MM.dd 'at' HH:mm:ss z", Locale.getDefault()) }
+
+    override fun doWork(): Result {
+        makeStatusNotification("Saving image", applicationContext)
+        sleep()
+
+        val resolver = applicationContext.contentResolver
+
+        return try {
+            val resourceUri = inputData.getString(KEY_IMAGE_URI)
+            val bitmap = BitmapFactory.decodeStream(resolver.openInputStream(Uri.parse(resourceUri)))
+            val imageUrl = MediaStore.Images.Media.insertImage(resolver, bitmap, title, dateFormatter.format(Date()))
+
+            if(!imageUrl.isNullOrEmpty()) {
+                val output = workDataOf(KEY_IMAGE_URI to imageUrl)
+
+                Result.success(output)
+            }
+            else {
+                Timber.e("Writing to MediaStore failed")
+                Result.failure()
+            }
+        } catch (exception: Exception) {
+            Timber.e(exception)
+            Result.failure()
+        }
+    }
+}
